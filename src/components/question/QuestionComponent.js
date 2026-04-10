d4e8096206b495c356371df15026ad22ac67f67f
K.Vaishnavi <23wh1a0511@bvrithyderabad.edu.in.com>
2026-04-10 21:40:07 +0530

    updated QuestionComponent.js

--- src/components/question/QuestionComponent.js
+++ src/components/question/QuestionComponent.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import {useNavigate} from 'react-router-dom';
 
 function QuestionComponent() {
-  const [tasks, setTasks] = useState(null); 
+  const [tasks, setTasks] = useState([]); 
   const navigate = useNavigate(); 
   useEffect(() => { 
     fetch("http://bvrithcloud.com/api/tasks", {
@@ -13,15 +13,27 @@ function QuestionComponent() {
         "x-student-id":"23WH1A0511"
       }
     })
-    .then(response => response.json())
-    .then(data => setTasks(data))
+    .then((res) => res.json())
+    .then(data => {
+      console.log("API DATA:", data);
+      if (Array.isArray(data)) {
+        setTasks(data);   
+      }else if(data.tasks) {
+        setTasks(data.tasks); 
+      }else if (data.data) {
+        setTasks(data.data); 
+      }else{
+        setTasks([]);
+      }
+    })
     .catch(error => console.error("Error fetching tasks:", error));
   }, []); 
   return (
     <div>
       <h2> Task List</h2>
       <button onClick={() => navigate('/add-task')}>Add Task</button>
-      {tasks.map((task) => (
+      {Array.isArray(tasks) &&
+       tasks.map((task) => (
         <div key={task._id}>
           <h3>{task.title}</h3> 
           <p>{task.description}</p>
